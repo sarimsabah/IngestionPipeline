9f168e2f2099aaabc3ddaa5bfc1946e51442d682
Sarim Sabah <sarimsabah@gmail.com>
2025-11-12 19:12:02 +0530

    Revert "added env"
    This reverts commit 80fd6cf84c07db6d1ed58cbd3bce2dd0374e8647.

--- MyApiApp/Program.cs
+++ MyApiApp/Program.cs
@@ -7,10 +7,6 @@ using MyApiApp.BackgroundJobs;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.EntityFrameworkCore;
-using DotNetEnv;
-
-// Load environment variables from .env file
-Env.Load();
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -19,14 +15,8 @@ builder.Services.AddOpenApi();
 builder.Services.AddSingleton<DummyDataService>();
 
 // Add Database
-var connectionString = $"Host={Environment.GetEnvironmentVariable("DB_HOST")};" +
-                      $"Port={Environment.GetEnvironmentVariable("DB_PORT")};" +
-                      $"Database={Environment.GetEnvironmentVariable("DB_NAME")};" +
-                      $"Username={Environment.GetEnvironmentVariable("DB_USERNAME")};" +
-                      $"Password={Environment.GetEnvironmentVariable("DB_PASSWORD")};";
-
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseNpgsql(connectionString));
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 // Add Repositories
 builder.Services.AddScoped<ICustomerRepository, CustomerRepository>();
